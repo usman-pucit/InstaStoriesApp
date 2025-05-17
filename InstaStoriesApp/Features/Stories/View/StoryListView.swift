f2ae303558e5f24661ab91884da04224877b46fd
Muhammad Usman <usmans1020@gmail.com>
2025-05-17 14:24:34 +0200

    feat: Implement pagination logic

--- InstaStoriesApp/Features/Stories/View/StoryListView.swift
+++ InstaStoriesApp/Features/Stories/View/StoryListView.swift
@@ -8,14 +8,115 @@
 import SwiftUI
 
 struct StoryListView: View {
+    @State private var viewModel = StoryListViewModel()
+    @State private var reloadTrigger = UUID()
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                contentView
+            }
+            .navigationBarTitleDisplayMode(.large)
+            .navigationTitle("Stories")
+        }
+        .background(Color(.systemBackground))
+        .task(id: reloadTrigger) {
+            await viewModel.fetchStories()
+        }
+    }
+    
+    // MARK: - Private Views
+    
+    @ViewBuilder
+    private var contentView: some View {
+        switch viewModel.viewState {
+        case .idle, .loading:
+            LoadingView()
+        case .error(let error):
+            errorView(with: error)
+        case .result(let stories):
+            StoriesView(stories: stories, onAppear: { story in
+                Task {
+                    await viewModel.loadMoreItemsIfNeeded(currentItem: story)
+                }
+            })
+        case .empty:
+            emptyView
+        }
+    }
+    
+    private var emptyView: some View {
+        GenericErrorView(errorMessage: "No stories available") {
+            reloadTrigger = UUID()
+        }
+    }
+    
+    private func errorView(with error: Error) -> some View {
+        GenericErrorView(errorMessage: error.localizedDescription) {
+            reloadTrigger = UUID()
+        }
+    }
+}
+
+
+// MARK: - Private Views
+private struct StoriesView: View {
+    private let stories: [UI.Story.User]
+    private var onAppear: (UI.Story.User) -> Void
+    
+    init(
+        stories: [UI.Story.User],
+        onAppear: @escaping (UI.Story.User) -> Void
+    ) {
+        self.stories = stories
+        self.onAppear = onAppear
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 12) {
+                ForEach(stories) { story in
+                    StoryCell(story: story, isSeen: false)
+                        .onAppear {
+                            onAppear(story)
+                        }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+// MARK: - Private Views
+private struct StoryCell: View {
+    let story: UI.Story.User
+    let isSeen: Bool
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AsyncImage(url: URL(string: story.profilePictureUrl)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                Color.gray
+            }
+            .frame(width: 60, height: 60)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(
+                        isSeen ? Color.gray : Color.blue,
+                        lineWidth: 2
+                    )
+            )
+            
+            Text(story.name)
+                .font(.caption)
+                .foregroundColor(.primary)
+                .lineLimit(1)
         }
-        .padding()
+        .frame(width: 70)
     }
 }
 
--- InstaStoriesApp/Features/Stories/View/StoryListViewModel.swift
+++ InstaStoriesApp/Features/Stories/View/StoryListViewModel.swift
@@ -10,24 +10,41 @@ import Foundation
 // Define a protocol for the view model
 protocol StoryListViewModelType {
     func fetchStories() async
+    func loadMoreItemsIfNeeded(currentItem: UI.Story.User?) async
+    func refresh() async
 }
 
 // Mark: - ViewModel
 @Observable
 final class StoryListViewModel {
-    enum State {
+    enum State: Equatable {
         case idle
         case loading
         case result([UI.Story.User])
         case empty
         case error(Error)
+        
+        static func == (lhs: State, rhs: State) -> Bool {
+            switch (lhs, rhs) {
+            case (.idle, .idle), (.loading, .loading), (.empty, .empty):
+                return lhs == rhs
+            default:
+                return true
+            }
+        }
     }
     
     private(set) var viewState: State = .idle
-        
+    
     @ObservationIgnored
     private let useCase: StoryListUseCaseType
+    private var currentPage = 0
+    private var totalPages = 0
     
+    private var canLoadMorePages = true
+    private var totalStories: [UI.Story.User] = []
+    
+    // MARK: - Init
     init(
         useCase: StoryListUseCaseType = StoryListUseCase()
     ) {
@@ -36,17 +53,48 @@ final class StoryListViewModel {
 }
 
 extension StoryListViewModel: StoryListViewModelType {
-    @MainActor
     func fetchStories() async {
-        viewState = .loading
+//        viewState = .loading
         
         do {
-            let stories = try await useCase.fetchStories()
-            let users = stories.flatMap { $0.users }
-            let normalizedUsers = users.map { $0.normalize()} // Normalize the response
-            viewState = .result(normalizedUsers)
+            let result = try await useCase.fetchStories()
+            canLoadMorePages = result.count > currentPage // Check if there are more pages to load
+            
+            if result.count > currentPage {
+                let currentPage = result[currentPage]
+                let users = currentPage.users
+                let normalizedUsers = users.map { $0.normalize()} // Normalize the response
+                self.totalStories += normalizedUsers // Append the new users to the total stories
+                
+                await MainActor.run {
+                    viewState = .result(self.totalStories)
+                }
+            }
+            
         } catch {
-            viewState = .error(error)
+            await MainActor.run {
+                viewState = .error(error)
+            }
         }
+    }
+    
+    func loadMoreItemsIfNeeded(currentItem: UI.Story.User?) async {
+        // Implement pagination logic
+        guard let item = currentItem,
+              item.id == totalStories.last?.id,
+              canLoadMorePages
+        else {
+            return
+        }
+        
+        currentPage += 1
+        await fetchStories()
+    }
+    
+    func refresh() async {
+        currentPage = 1
+        canLoadMorePages = true
+        totalStories = []
+        await fetchStories()
     }
 }
